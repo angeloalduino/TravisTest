a69d21705bb99fe381837698a1b9750c080bf112
Angelo Alduino <angelo.alduino@aenetworks.com>
2015-12-16 10:37:17 -0500

    Initial Commit

---
+++ TravisTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TravisTest
+//
+//  Created by Alduino, Angelo on 12/16/15.
+//  Copyright © 2015 pixelparts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TravisTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravisTest
+//
+//  Created by Alduino, Angelo on 12/16/15.
+//  Copyright © 2015 pixelparts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
